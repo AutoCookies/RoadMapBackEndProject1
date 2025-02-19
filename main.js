b42c5edc909a42c13e84eedfc1d553b7a1d84e79
Cookiescooker <111939726+AutoCookies@users.noreply.github.com>
2025-02-19 22:45:35 +0700

    Final Version

--- main.js
+++ main.js
@@ -1,44 +1,64 @@
-import { addTask, updateTask, deleteTask, markATask, showAll, showDoneTasks, showNotDoneTasks, showInProgressTask } from "./Utils/utils.js";
-import readlineSync from 'readline-sync';
+#!/usr/bin/env node
+import { Command } from "commander";
+import { 
+    addTask, updateTask, deleteTask, markATask, 
+    showAll, showDoneTasks, showInProgressTasks 
+} from "./Utils/utils.js";
 
-function main() {
-    while (true) {
-        console.log("\nTask Tracker CLI");
-        console.log("1. Add Task");
-        console.log("2. Delete Task");
-        console.log("3. Update Task");
-        console.log("4. Mark a Task")
-        console.log("5. Show All Tasks");
-        console.log("6. List all done task");
-        console.log("7. Show not Done task");
-        console.log("8. Show not in_progress task");
-        console.log("0. Exit");
+const program = new Command();
 
-        let choice = readlineSync.question("Choose an option: ").trim();
+program
+    .name("task-cli")
+    .description("A simple task tracker CLI")
+    .version("1.0.0");
 
-        if (choice === "1") {
-            addTask();
-        } else if (choice === "2") {
-            deleteTask();
-        } else if (choice === "3") {
-            updateTask();
-        } else if (choice === "4") {
-            markATask();
-        } else if (choice === "5") {
-            showAll();
-        } else if (choice === "6") {
-            showDoneTasks();
-        } else if (choice === "7") {
-            showNotDoneTasks();
-        } else if (choice === "8") {
-            showInProgressTask();
-        } else if (choice === "0") {
-            console.log("Program ended.");
-            return;
-        } else {
-            console.log("Invalid choice. Please enter 1, 2, 3, or 0.");
-        }
-    }
-}
+program
+    .command("add <description>")
+    .description("Add a new task")
+    .action((description) => {
+        addTask(description);
+    });
 
-main();
+program
+    .command("update <id> <description>")
+    .description("Update task description")
+    .action((id, description) => {
+        updateTask(parseInt(id), description);
+    });
+
+program
+    .command("delete <id>")
+    .description("Delete a task")
+    .action((id) => {
+        deleteTask(parseInt(id));
+    });
+
+program
+    .command("mark-done <id>")
+    .description("Mark task as done")
+    .action((id) => {
+        markATask(parseInt(id));
+    });
+
+program
+    .command("list")
+    .description("List all tasks")
+    .action(() => {
+        showAll();
+    });
+
+program
+    .command("list-done")
+    .description("List all done tasks")
+    .action(() => {
+        showDoneTasks();
+    });
+
+program
+    .command("list-in-progress")
+    .description("List all in-progress tasks")
+    .action(() => {
+        showInProgressTasks();
+    });
+
+program.parse(process.argv);
